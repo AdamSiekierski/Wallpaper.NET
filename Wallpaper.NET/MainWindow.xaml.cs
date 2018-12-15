15806e865e3fd4ded058c39d6071893657a49249
adam.siekiera <adam.siekiera@outlook.com>
2018-12-15 10:59:12 +0100

    First pull

---
+++ Wallpaper.NET/MainWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Diagnostics;
+using Tray;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.IO;
+
+namespace Wallpaper.NET
+{
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        TrayClass trayClass = new TrayClass();
+
+        // When the window loads
+        void OnLoad(object sender, RoutedEventArgs e)
+        {
+            if (Properties.Settings.Default.DefaultWallpaper == "") // On first launch
+            {
+                firstLaunch();
+            }
+            else
+            {
+                setWallpaper();
+                trayClass.ShowIcon();
+            }
+        }
+
+        // Creme de la creme of .NET programming - .dll files import
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int SystemParametersInfo(int uAction,
+            int uParam, string lpvParam, int fuWinIni);
+        private static readonly int SPI_SETDESKWALLPAPER = 0x14;
+        private static readonly int SPIF_UPDATEINIFILE = 0x01;
+        private static readonly int SPIF_SENDWININICHANGE = 0x02;
+
+        // Function to set the prorper wallpaper, and controll the interface
+        private void setWallpaper()
+        {
+            // Get the date
+            DateTime today = DateTime.Today;
+            ///DateTime today = new DateTime(2018, 10, 22);
+            string month = today.ToString("MM");
+            float day = float.Parse(today.ToString("dd"));
+
+            // Set the date label to current date
+            todayLabel.Content = today.ToString("dd.MM.yyyy");
+
+            // Set the wallpaper appropriate to the date
+            /// Christmas
+            if (month == "12" && day <= 24)
+            {
+                string christmasPath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "Christmas"); // Create the path
+                Properties.Resources.Christmas.Save(christmasPath, System.Drawing.Imaging.ImageFormat.Png); // Save a resource to Temp directory
+                // User interface
+                textBox.Text = "Christmas";
+                image.Source = new BitmapImage(new Uri(christmasPath));
+                // Set the wallpaper
+                SetDesktopWallpaper(christmasPath);
+            }
+            /// Other dates
+            else
+            {
+                textBox.Text = "Default WP";
+                image.Source = new BitmapImage(new Uri(Properties.Settings.Default.DefaultWallpaper, UriKind.RelativeOrAbsolute));
+                SetDesktopWallpaper(Properties.Settings.Default.DefaultWallpaper);
+            }
+        }
+
+
+        // Quick code, to set the wallpaper
+        private void SetDesktopWallpaper(string filename)
+        {
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, filename,
+                SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+
+
+        // On the first launch
+        private void firstLaunch()
+        {
+            // Show the message box to ask for the default wallpaper
+            MessageBoxResult msgbox = MessageBox.Show("On first launch you need to choose the default wallpaper", "Default wallpaper", MessageBoxButton.OK, MessageBoxImage.Question);
+
+            // Call the function for the default wallpaper file dialog; when didn't choose anything, close the app
+            chooseWallpaper(true);
+        }
+
+        // Choose the default wallpeper dialog
+        private void chooseWallpaper(bool FirstLaunch)
+        {
+            OpenFileDialog dlg = new OpenFileDialog(); // Create the file dialog object
+
+            dlg.Filter = "Image files|*.jpeg; *.png; *.jpg; *.gif"; // Set filters for image file types
+            dlg.Title = "Choose default wallpaper (Only the first use!)"; // Set the title of dialog
+
+            Nullable<bool> result = dlg.ShowDialog(); // Show the dialog box
+            if (result == true)
+            {
+                Properties.Settings.Default.DefaultWallpaper = dlg.FileName;
+                Properties.Settings.Default.Save();
+                setWallpaper();
+            }
+            else
+            {
+                if (FirstLaunch == true)
+                {
+                    Application.Current.Shutdown();
+                }
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            chooseWallpaper(false);
+            setWallpaper();
+        }
+
+        // Don't close app, when X button clicked
+        void HideNotClose(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+    }
+}
---
+++ Wallpaper.NET/TrayClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+using Wallpaper.NET;
+
+namespace Tray
+{
+    public class TrayClass
+    {
+
+        private System.Windows.Forms.NotifyIcon _notifyIcon;
+        private System.Windows.Forms.ContextMenu _contextMenu;
+
+        public void ShowIcon()
+        {
+            // Create the objects
+            _notifyIcon = new System.Windows.Forms.NotifyIcon();
+            _contextMenu = new System.Windows.Forms.ContextMenu();
+
+            // Prepare the icon
+            _notifyIcon.Icon = Wallpaper.NET.Properties.Resources.TrayIcon;
+            _notifyIcon.ContextMenu = _contextMenu;
+            _notifyIcon.Click += new EventHandler(NotifyIcon1_Click);
+
+            // Prepare the context menu
+            _contextMenu.MenuItems.Add("Show", Item1_onClick);
+            _contextMenu.MenuItems.Add("Close", Item2_onClick);
+
+            // Launch the icon
+            _notifyIcon.Visible = true;
+
+        }
+
+        private void Item1_onClick(object sender, EventArgs e)
+        {
+            Application.Current.MainWindow.Show();
+        }
+        private void Item2_onClick(object sender, EventArgs e)
+        {
+            MessageBoxResult clsMsgBox = MessageBox.Show("Are you sure, that you would like to close the app?", "Close", MessageBoxButton.YesNo, MessageBoxImage.None);
+            if (clsMsgBox == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+        private void NotifyIcon1_Click(object sender, EventArgs e)
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
